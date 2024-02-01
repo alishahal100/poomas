493abae11bad80da94f8e0898bd578920cd01044
Ali Shahal <alishahal9656@gmail.com>
2024-02-01 23:10:04 +0530

    commit initial files

---
+++ src/components/whatsApp.tsx
@@ -0,0 +1,32 @@
+// WhatsAppChatButton.tsx
+'use client'
+import { formatPrice } from '@/lib/utils';
+interface Product {
+    name: string;
+    price: number;
+    description: string | null | undefined;
+  }
+  
+  interface WhatsAppChatButtonProps {
+    product: Product;
+  }
+  
+
+const WhatsAppChatButton: React.FC<WhatsAppChatButtonProps> = ({ product }) => {
+  const openWhatsAppChat = () => {
+    const message = `Check out this product: ${product.name}\nPrice: ${formatPrice(product.price)}\nDescription: ${product.description}`;
+    const whatsappURL = `https://wa.me/7356323421?text=${encodeURIComponent(message)}`;
+    window.open(whatsappURL, '_blank');
+  };
+
+  return (
+    <button
+      onClick={openWhatsAppChat}
+      className='inline-flex items-center justify-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+    >
+      Chat for Details
+    </button>
+  );
+};
+
+export default WhatsAppChatButton;
---
+++ src/lib/utils.ts
@@ -0,0 +1,69 @@
+import { type ClassValue, clsx } from 'clsx'
+import { Metadata } from 'next'
+import { twMerge } from 'tailwind-merge'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatPrice(
+  price: number | string,
+  options: {
+    currency?: 'AED' | 'EUR' | 'GBP' | 'BDT'
+    notation?: Intl.NumberFormatOptions['notation']
+  } = {}
+) {
+  const { currency = 'AED', notation = 'compact' } = options
+
+  const numericPrice =
+    typeof price === 'string' ? parseFloat(price) : price
+
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency,
+    notation,
+    maximumFractionDigits: 2,
+  }).format(numericPrice)
+}
+
+export function constructMetadata({
+  title = ' Poomas - Buy and Sell Properties, Shops, and Vehicles in the UAE | Poomas',
+  description = 'Discover a wide selection of  properties, shops, and vehicles for sale in the UAE. Buy or sell with ease on Poomas!',
+  image = '/logo.jpg',
+  icons = '/favicon.ico',
+  noIndex = false,
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+  noIndex?: boolean
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+          width: 1200,
+          height: 630,
+          alt: 'Poomas - Your Destination for High-Quality Properties, Shops, and Vehicles in the UAE',
+        },
+      ],
+      siteName: 'Poomas',
+   
+    },
+    
+    icons,
+    metadataBase: new URL('https://digitalhippo.up.railway.app'),
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  }
+}
